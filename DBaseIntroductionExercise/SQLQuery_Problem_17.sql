a185ba92d630432fc7d05a2c51343674c8eaa775
Mironov <mironov5712s@gmail.com>
2021-01-15 14:22:10 +0200

    Create SQLQuery_Problem_17.sql

---
+++ DBaseIntroductionExercise/SQLQuery_Problem_17.sql
@@ -0,0 +1,39 @@
+--CREATE DATABASE SoftUni
+
+-- Data from Problem 16. Create SoftUni Database
+
+CREATE TABLE Towns 
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL, 
+	[Name] NVARCHAR(50) NOT NULL
+)
+CREATE TABLE Addresses 
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL, 
+	AddressText NVARCHAR(200) NOT NULL, 
+	TownId INT FOREIGN KEY REFERENCES Towns(Id) NOT NULL
+)
+CREATE TABLE Departments 
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL, 
+	[Name] NVARCHAR(50) NOT NULL
+)
+CREATE TABLE Employees 
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL, 
+	FirstName NVARCHAR(50) NOT NULL, 
+	MiddleName NVARCHAR(50), 
+	LastName NVARCHAR(50) NOT NULL, 
+	JobTitle NVARCHAR(50) NOT NULL, 
+	DepartmentId INT FOREIGN KEY REFERENCES Departments(Id) NOT NULL, 
+	HireDate DATE NOT NULL, 
+	Salary DECIMAL(7, 2) NULL, 
+	AddressId INT FOREIGN KEY REFERENCES Addresses(Id) NOT NULL
+)
+-- Problem 17. Backup Database
+USE master
+BACKUP DATABASE SoftUni TO DISK = 'E:\Soft-Uni\SQL Backup Data Base\SoftUni-backup.bak'
+
+DROP DATABASE SoftUni
+
+RESTORE DATABASE SoftUni FROM DISK = 'E:\Soft-Uni\SQL Backup Data Base\SoftUni-backup.bak'
