75248d68b3f26044e51117f5328aaba63db41e46
Mironov <mironov5712s@gmail.com>
2021-01-27 02:18:23 +0200

    Update SQLQuery_Problem_06_11.sql

--- 04. BuiltInFunctions/Task_Solutions/SQLQuery_Problem_06_11.sql
+++ 04. BuiltInFunctions/Task_Solutions/SQLQuery_Problem_06_11.sql
@@ -41,3 +41,13 @@ SELECT EmployeeID, FirstName, LastName, Salary,
     WHERE Salary >= 10000 AND Salary <= 50000
     ORDER BY Salary DESC
 
+-- 11. Find All Employees with Rank 2
+
+SELECT *
+	FROM 
+		(SELECT EmployeeID, FirstName, LastName, Salary,
+			DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+			FROM Employees
+			WHERE (Salary >= 10000 AND Salary <= 50000)) AS TempTable
+	WHERE RANK = 2
+	ORDER BY Salary DESC
