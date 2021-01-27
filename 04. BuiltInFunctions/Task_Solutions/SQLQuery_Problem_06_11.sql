7ad860e3562acdf2480f5ea788dbf59688cdd830
Mironov <mironov5712s@gmail.com>
2021-01-27 02:15:02 +0200

    Update SQLQuery_Problem_06_11.sql

--- 04. BuiltInFunctions/Task_Solutions/SQLQuery_Problem_06_11.sql
+++ 04. BuiltInFunctions/Task_Solutions/SQLQuery_Problem_06_11.sql
@@ -17,3 +17,12 @@ SELECT *
 	FROM Towns
 	WHERE NOT (LEFT([Name], 1) = 'R' OR LEFT([Name], 1) = 'B' OR LEFT([Name], 1) = 'D')
 	ORDER BY [Name] ASC
+
+-- 08. Create View Employees Hired After
+
+CREATE VIEW V_EmployeesHiredAfter2000 AS
+	SELECT FirstName, LastName
+		FROM Employees
+		WHERE YEAR(HireDate) > 2000
+
+SELECT * FROM V_EmployeesHiredAfter2000
