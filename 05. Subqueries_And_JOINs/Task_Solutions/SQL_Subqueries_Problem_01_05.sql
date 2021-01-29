f3452fc72bb01064ef05bb1b36785f0c3dafb8a6
Mironov <mironov5712s@gmail.com>
2021-01-29 17:52:39 +0200

    Update SQL_Subqueries_Problem_01_05.sql

--- 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_01_05.sql
+++ 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_01_05.sql
@@ -5,3 +5,11 @@ SELECT TOP(5) e.EmployeeID, e.JobTitle, a.AddressID, a.AddressText
 	JOIN [Addresses] AS a ON e.AddressID = a.AddressID
 	ORDER BY a.AddressID
 
+-- 02. Addresses with Towns
+
+SELECT TOP(50) e.FirstName, e.LastName, t.[Name] AS Town, a.AddressText
+	FROM [Employees] AS e
+	JOIN [Addresses] AS a ON e.AddressID = a.AddressID
+	JOIN [Towns] AS t ON a.TownID = t.TownID
+	ORDER BY e.FirstName, e.LastName
+
