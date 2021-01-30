a54cc1e183e2b801c52185ccdbc441724006eef2
Mironov <mironov5712s@gmail.com>
2021-01-30 21:42:47 +0200

    Update SQL_Subqueries_Problem_06_11.sql

--- 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_06_11.sql
+++ 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_06_11.sql
@@ -49,3 +49,14 @@ SELECT e.EmployeeID, e.FirstName, e.ManagerID, m.FirstName AS ManagerName
 	WHERE e.ManagerID IN (3, 7)
 	ORDER BY e.EmployeeID
 
+-- 10. Employees Summary
+
+SELECT TOP(50) e.EmployeeID,
+		CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
+		CONCAT(m.FirstName, ' ', m.LastName) AS ManagerName,
+		d.[Name] AS DepartmentName
+	FROM [Employees] AS e
+	JOIN [Employees] AS m ON e.ManagerID = m.EmployeeID
+	JOIN [Departments] AS d ON e.DepartmentID = d.DepartmentID
+	ORDER BY e.EmployeeID
+
