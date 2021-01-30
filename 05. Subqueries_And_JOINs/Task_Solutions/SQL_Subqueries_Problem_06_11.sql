7e599ea515e48df3b21102458fbd092265f88054
Mironov <mironov5712s@gmail.com>
2021-01-30 21:37:59 +0200

    Update SQL_Subqueries_Problem_06_11.sql

--- 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_06_11.sql
+++ 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_06_11.sql
@@ -12,3 +12,12 @@ SELECT e.FirstName, e.LastName, e.HireDate, d.[Name] AS DeptName
 	WHERE (e.HireDate > '1.1.1999')
 	ORDER BY e.HireDate ASC
 
+-- 07. Employees With Project
+
+SELECT TOP(5) e.EmployeeID, e.FirstName, p.[Name] AS ProjectName
+	FROM [Employees] AS e
+	JOIN [EmployeesProjects] AS ep ON e.EmployeeID = ep.EmployeeID
+	JOIN [Projects] AS p ON ep.ProjectID = p.ProjectID
+	WHERE p.EndDate IS NULL AND p.StartDate > '2002-08-13'
+	ORDER BY e.EmployeeID
+	
