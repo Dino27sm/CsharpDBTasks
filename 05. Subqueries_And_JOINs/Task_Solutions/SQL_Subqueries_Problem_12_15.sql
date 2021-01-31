229a0fc36bb7f5a0b8a2843e029c224686bbcbd0
Mironov <mironov5712s@gmail.com>
2021-01-31 07:53:13 +0200

    Update SQL_Subqueries_Problem_12_15.sql

--- 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_12_15.sql
+++ 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_12_15.sql
@@ -8,3 +8,11 @@ SELECT c.CountryCode, m.MountainRange, p.PeakName, p.Elevation
 	WHERE c.CountryCode = 'BG' AND p.Elevation > 2835
 	ORDER BY p.Elevation DESC
 
+-- 13. Count Mountain Ranges
+
+SELECT c.CountryCode, COUNT(c.CountryCode) AS MountainRange
+	FROM [MountainsCountries] AS mc
+	JOIN [Countries] AS c ON mc.CountryCode = c.CountryCode
+	JOIN [Mountains] AS m ON mc.MountainId = m.Id
+	WHERE c.CountryCode IN ('US', 'RU', 'BG')
+	GROUP BY c.CountryCode
