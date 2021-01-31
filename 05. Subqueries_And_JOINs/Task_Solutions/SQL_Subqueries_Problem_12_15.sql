4fa91857dc14aa9f22d4b951720ddc8529c3709d
Mironov <mironov5712s@gmail.com>
2021-01-31 08:21:02 +0200

    Update SQL_Subqueries_Problem_12_15.sql

--- 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_12_15.sql
+++ 05. Subqueries_And_JOINs/Task_Solutions/SQL_Subqueries_Problem_12_15.sql
@@ -30,3 +30,10 @@ SELECT TOP(5) c.CountryName, r.RiverName
 	FULL JOIN [Rivers] AS r ON cr.RiverId = r.Id
 	WHERE c.ContinentCode = 'AF'
 	ORDER BY c.CountryName
+	
+SELECT TOP(5) c.CountryName, r.RiverName
+	FROM [Countries] AS c
+	LEFT JOIN [CountriesRivers] AS cr ON cr.CountryCode = c.CountryCode
+	LEFT JOIN [Rivers] AS r ON cr.RiverId = r.Id
+	WHERE c.ContinentCode = 'AF'
+	ORDER BY c.CountryName
