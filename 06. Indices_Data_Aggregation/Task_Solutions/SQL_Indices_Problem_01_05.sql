b2b3dffffe44158cebe0a502d4275e469f6a4b2e
Mironov <mironov5712s@gmail.com>
2021-02-03 14:16:13 +0200

    Update SQL_Indices_Problem_01_05.sql

--- 06. Indices_Data_Aggregation/Task_Solutions/SQL_Indices_Problem_01_05.sql
+++ 06. Indices_Data_Aggregation/Task_Solutions/SQL_Indices_Problem_01_05.sql
@@ -21,3 +21,9 @@ SELECT TOP(2) [DepositGroup]
 	FROM [WizzardDeposits]
 	GROUP BY [DepositGroup]
 	ORDER BY AVG([MagicWandSize])
+
+-- 05. Deposits Sum
+
+SELECT [DepositGroup], SUM([DepositAmount]) AS TotalSum
+	FROM [WizzardDeposits]
+	GROUP BY [DepositGroup]
