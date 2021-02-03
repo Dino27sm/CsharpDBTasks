8c6220ea1cab0f4d096191b32cf932235a24eeb5
Mironov <mironov5712s@gmail.com>
2021-02-03 14:14:22 +0200

    Create SQL_Indices_Problem_01_05.sql

---
+++ 06. Indices_Data_Aggregation/Task_Solutions/SQL_Indices_Problem_01_05.sql
@@ -0,0 +1,17 @@
+--USE [Gringotts]
+-- 01. Records’ Count
+
+SELECT COUNT(*) AS [Count]
+	FROM [WizzardDeposits]
+
+-- 02. Longest Magic Wand
+
+SELECT MAX([MagicWandSize]) AS LongestMagicWand
+	FROM [WizzardDeposits]
+
+-- 03. Longest Magic Wand per Deposit Groups
+
+SELECT [DepositGroup], MAX([MagicWandSize]) AS LongestMagicWand
+	FROM [WizzardDeposits]
+	GROUP BY [DepositGroup]
+
