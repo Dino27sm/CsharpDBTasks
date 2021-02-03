7160f5ba7f381a8d80d0a4fbe7dc296a4b358fa8
Mironov <mironov5712s@gmail.com>
2021-02-03 14:20:07 +0200

    Update SQL_Indices_Problem_06_10.sql

--- 06. Indices_Data_Aggregation/Task_Solutions/SQL_Indices_Problem_06_10.sql
+++ 06. Indices_Data_Aggregation/Task_Solutions/SQL_Indices_Problem_06_10.sql
@@ -5,3 +5,16 @@ SELECT [DepositGroup], SUM([DepositAmount]) AS TotalSum
 	GROUP BY [DepositGroup], [MagicWandCreator]
 	HAVING [MagicWandCreator] = 'Ollivander family'
 
+-- 07. Deposits Filter
+
+SELECT *
+	FROM 
+		(
+		SELECT [DepositGroup], SUM([DepositAmount]) AS TotalSum
+			FROM [WizzardDeposits]
+			GROUP BY [DepositGroup], [MagicWandCreator]
+			HAVING [MagicWandCreator] = 'Ollivander family'
+		)
+	AS TempTable
+	WHERE TempTable.TotalSum < 150000
+	ORDER BY TotalSum DESC
