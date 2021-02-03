996132e0183989c327face28db168397a68cb60f
Mironov <mironov5712s@gmail.com>
2021-02-03 14:18:58 +0200

    Create SQL_Indices_Problem_06_10.sql

---
+++ 06. Indices_Data_Aggregation/Task_Solutions/SQL_Indices_Problem_06_10.sql
@@ -0,0 +1,7 @@
+-- 06. Deposits Sum for Ollivander Family
+
+SELECT [DepositGroup], SUM([DepositAmount]) AS TotalSum
+	FROM [WizzardDeposits]
+	GROUP BY [DepositGroup], [MagicWandCreator]
+	HAVING [MagicWandCreator] = 'Ollivander family'
+
