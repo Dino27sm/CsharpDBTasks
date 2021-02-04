6a56b8b619796f15d53df567c68e877cbffc56a2
Mironov <mironov5712s@gmail.com>
2021-02-04 09:48:02 +0200

    Update SQL_Indices_Problem_06_10.sql

--- 06. Indices_Data_Aggregation/Task_Solutions/SQL_Indices_Problem_06_10.sql
+++ 06. Indices_Data_Aggregation/Task_Solutions/SQL_Indices_Problem_06_10.sql
@@ -58,3 +58,8 @@ SELECT DISTINCT LEFT(FirstName, 1) AS FirstLetter
 	WHERE [DepositGroup] = 'Troll Chest'
 	ORDER BY FirstLetter
 ------------------------------
+
+SELECT DISTINCT LEFT(FirstName, 1) AS FirstLetter
+	FROM [WizzardDeposits]
+	WHERE [DepositGroup] = 'Troll Chest'
+-------------------------------
