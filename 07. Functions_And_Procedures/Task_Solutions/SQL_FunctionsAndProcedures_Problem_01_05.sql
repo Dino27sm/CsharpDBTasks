8ecf10944580a1fb3f15aec899ffdaa5260f6d6f
Mironov <mironov5712s@gmail.com>
2021-02-07 15:34:30 +0200

    Create SQL_FunctionsAndProcedures_Problem_01_05.sql

---
+++ 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_01_05.sql
@@ -0,0 +1,19 @@
+--USE SoftUni
+-- 01. Employees with Salary Above 35000
+
+CREATE OR ALTER PROC usp_GetEmployeesSalaryAbove35000
+AS
+	SELECT FirstName AS [First Name], LastName AS [Last Name]
+		FROM Employees
+		WHERE Salary > 35000
+GO
+
+CREATE PROC usp_GetEmployeesSalaryAbove35000
+AS
+BEGIN
+	SELECT FirstName AS [First Name], LastName AS [Last Name]
+		FROM Employees
+		WHERE Salary > 35000
+END
+EXEC usp_GetEmployeesSalaryAbove35000
+
