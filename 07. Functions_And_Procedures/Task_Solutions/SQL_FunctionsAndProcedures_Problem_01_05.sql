315b9bb0b166001dd6bcd89ead55503ff86187b6
Mironov <mironov5712s@gmail.com>
2021-02-07 15:38:25 +0200

    Update SQL_FunctionsAndProcedures_Problem_01_05.sql

--- 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_01_05.sql
+++ 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_01_05.sql
@@ -49,3 +49,19 @@ AS
 GO
 EXEC usp_GetEmployeesFromTown 'Sofia'
 
+-- 05. Salary Level Function
+
+CREATE OR ALTER FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS VARCHAR(30)
+AS
+BEGIN
+	DECLARE @SalaryLevel VARCHAR(30)
+	IF (@salary < 30000)
+		SET @SalaryLevel = 'Low'
+	ELSE IF (@salary >= 30000 AND @salary <= 50000)
+		SET @SalaryLevel = 'Average'
+	ELSE IF (@salary > 50000)
+		SET @SalaryLevel = 'High'
+	RETURN @SalaryLevel
+END
+
