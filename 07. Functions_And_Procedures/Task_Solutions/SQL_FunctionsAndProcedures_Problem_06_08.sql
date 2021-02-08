1cf0b99f4750f35ede58b1ddcc26a4bc41f68b3a
Mironov <mironov5712s@gmail.com>
2021-02-08 09:11:05 +0200

    Update SQL_FunctionsAndProcedures_Problem_06_08.sql

--- 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_06_08.sql
+++ 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_06_08.sql
@@ -49,3 +49,21 @@ SET ManagerID=NULL
     ALTER TABLE Departments
         ALTER COLUMN ManagerID INT
 
+UPDATE Departments
+SET ManagerID=NULL
+    WHERE ManagerID IN (SELECT EmployeeID
+                            FROM Employees
+                            WHERE Employees.DepartmentID = @departmentId)
+
+DELETE
+    FROM Employees
+    WHERE DepartmentID = @departmentId
+
+DELETE
+    FROM Departments
+    WHERE DepartmentID = @departmentId
+
+SELECT COUNT(*)
+    FROM Employees
+    WHERE DepartmentID = @departmentId
+GO
