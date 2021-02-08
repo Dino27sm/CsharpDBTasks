44c7cc80694f7981a9ad0169742cb4d5b39b8b82
Mironov <mironov5712s@gmail.com>
2021-02-08 09:08:50 +0200

    Update SQL_FunctionsAndProcedures_Problem_06_08.sql

--- 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_06_08.sql
+++ 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_06_08.sql
@@ -8,3 +8,25 @@ AS
 GO
 EXEC usp_EmployeesBySalaryLevel 'High'
 
+-- 07. Define Function
+GO
+CREATE OR ALTER FUNCTION ufn_IsWordComprised(@setOfLetters NVARCHAR(MAX), @word NVARCHAR(MAX))
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @isContain BIT = 1;
+	DECLARE @counter INT = 1
+	WHILE(@counter <= LEN(@word))
+		BEGIN
+			IF (CHARINDEX(SUBSTRING(@word, @counter, 1), @setOfLetters) = 0)
+				BEGIN
+					SET @isContain = 0;
+					BREAK;
+				END
+			SET @counter += 1;
+		END
+	RETURN @isContain;
+END
+GO
+SELECT [dbo].[ufn_IsWordComprised]('oistmiahf', 'Sofia') AS Result
+
