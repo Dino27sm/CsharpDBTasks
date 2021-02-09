45d968d84797efff9efa3e0a5f2b7be292002b0a
Mironov <mironov5712s@gmail.com>
2021-02-09 08:17:42 +0200

    Update SQL_FunctionsAndProcedures_Problem_09_13.sql

--- 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_09_13.sql
+++ 07. Functions_And_Procedures/Task_Solutions/SQL_FunctionsAndProcedures_Problem_09_13.sql
@@ -7,3 +7,17 @@ AS
 
 -- EXEC usp_GetHoldersFullName
 
+-- 10. People with Balance Higher Than
+
+CREATE OR ALTER PROC usp_GetHoldersWithBalanceHigherThan (@totalBalance DECIMAL(17, 2))
+AS
+SELECT FirstName AS [First Name], LastName AS [Last Name]
+	FROM(SELECT FirstName, LastName, SUM(Balance) AS TotalBalance
+			FROM [AccountHolders] AS ah
+			JOIN [Accounts] AS acc ON acc.AccountHolderId = ah.Id
+			GROUP BY FirstName, LastName
+		) AS TempTable
+		WHERE TotalBalance > @totalBalance
+		ORDER BY FirstName, LastName
+
+-----------------------------------------------------
