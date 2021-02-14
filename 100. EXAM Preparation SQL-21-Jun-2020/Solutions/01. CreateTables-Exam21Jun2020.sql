193aa13325fd49192bb80a865529a07925a77728
Mironov <mironov5712s@gmail.com>
2021-02-14 05:02:25 +0200

    Create 01. CreateTables-Exam21Jun2020.sql

---
+++ 100. EXAM Preparation SQL-21-Jun-2020/Solutions/01. CreateTables-Exam21Jun2020.sql
@@ -0,0 +1,59 @@
+--CREATE DATABASE TripService
+--USE TripService
+
+CREATE TABLE Cities
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(20) NOT NULL,
+	CountryCode CHAR(2) NOT NULL
+)
+
+CREATE TABLE Hotels 
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(30) NOT NULL,
+	CityId INT REFERENCES Cities(Id) NOT NULL,
+	EmployeeCount INT NOT NULL,
+	BaseRate DECIMAL(17, 2) NULL
+)
+
+CREATE TABLE Accounts
+(
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(50) NOT NULL,
+	MiddleName NVARCHAR(20) NULL,
+	LastName NVARCHAR(50) NOT NULL,
+	CityId INT REFERENCES Cities NOT NULL,
+	BirthDate DATE NOT NULL,
+	Email VARCHAR(100) UNIQUE NOT NULL
+)
+
+CREATE TABLE Rooms
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Price DECIMAL(17, 2) NOT NULL,
+	[Type] NVARCHAR(20) NOT NULL,
+	Beds INT NOT NULL,
+	HotelId INT REFERENCES Hotels(Id) NOT NULL
+)
+
+CREATE TABLE Trips
+(
+	Id INT PRIMARY KEY IDENTITY,
+	RoomId INT REFERENCES Rooms(Id) NOT NULL,
+	BookDate DATE NOT NULL,
+	ArrivalDate DATE NOT NULL,
+	ReturnDate DATE NOT NULL,
+		CHECK (BookDate < ArrivalDate),
+		CHECK (ArrivalDate < ReturnDate),
+	CancelDate DATE NULL
+)
+
+CREATE TABLE AccountsTrips
+(
+	AccountId INT REFERENCES Accounts(Id) NOT NULL,
+	TripId INT REFERENCES Trips(Id) NOT NULL,
+	PRIMARY KEY(AccountId, TripId),
+	Luggage INT NOT NULL,
+		CHECK(Luggage >= 0)
+)
