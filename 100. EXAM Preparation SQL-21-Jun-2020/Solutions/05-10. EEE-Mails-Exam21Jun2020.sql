22eef449565f34599c482430ab3c7ba6334e9ed0
Mironov <mironov5712s@gmail.com>
2021-02-15 14:45:03 +0200

    Update 05-10. EEE-Mails-Exam21Jun2020.sql

--- 100. EXAM Preparation SQL-21-Jun-2020/Solutions/05-10. EEE-Mails-Exam21Jun2020.sql
+++ 100. EXAM Preparation SQL-21-Jun-2020/Solutions/05-10. EEE-Mails-Exam21Jun2020.sql
@@ -6,3 +6,13 @@ SELECT a.FirstName, a.LastName, FORMAT(a.BirthDate, 'MM-dd-yyyy') AS BirthDate, 
 	WHERE Email LIKE 'e%'
 	ORDER BY c.[Name] ASC
 
+-- 06. City Statistics
+
+SELECT temp.City, temp.Hotels
+	FROM (SELECT c.[Name] AS City, c.Id, h.CityId, COUNT(*) AS Hotels
+			FROM Cities AS c
+			JOIN Hotels AS h ON c.Id = h.CityId
+			GROUP BY c.[Name], c.Id, h.CityId) AS temp
+
+	ORDER BY temp.Hotels DESC, temp.City
+
