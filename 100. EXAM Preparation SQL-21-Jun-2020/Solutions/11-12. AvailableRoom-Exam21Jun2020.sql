1450605173f77dfdc7176a1881ae2d600727f14a
Mironov <mironov5712s@gmail.com>
2021-02-16 15:27:22 +0200

    Update 11-12. AvailableRoom-Exam21Jun2020.sql

--- 100. EXAM Preparation SQL-21-Jun-2020/Solutions/11-12. AvailableRoom-Exam21Jun2020.sql
+++ 100. EXAM Preparation SQL-21-Jun-2020/Solutions/11-12. AvailableRoom-Exam21Jun2020.sql
@@ -52,3 +52,37 @@ DECLARE @totalPrice DECIMAL(17, 2) = (@hotelBaseRate + @roomPrice) * @People;
 
 RETURN CONCAT('Room ', @roomID, ': ', @roomType, ' (', @roomBeds, ' beds) - $', @totalPrice);
 END
+
+--------------------------------------------------------------------------
+-- 12. Switch Room
+GO
+CREATE OR ALTER PROC usp_SwitchRoom(@TripId INT, @TargetRoomId INT)
+AS
+	DECLARE @tripHotelID INT = (SELECT r.HotelId
+								FROM Trips AS tr
+								JOIN Rooms AS r ON tr.RoomId = r.Id
+								WHERE tr.Id = @TripId)
+	DECLARE @tripRoomID INT = (SELECT RoomId
+								FROM Trips
+								WHERE Id = @TripId)
+	DECLARE @targetRoomHotelID INT = (SELECT HotelId FROM Rooms WHERE Id = @TargetRoomId)
+
+	IF (@targetRoomHotelID != @tripHotelID)
+		THROW 50001, 'Target room is in another hotel!', 1;
+
+	DECLARE @targetRoomBeds INT = (SELECT Beds FROM Rooms WHERE Id = @TargetRoomId);
+	DECLARE @tripPersons INT = (SELECT COUNT(AccountId) FROM AccountsTrips WHERE TripId = @TripId);
+
+	IF (@targetRoomBeds < @tripPersons)
+		THROW 50002, 'Not enough beds in target room!', 1;
+
+UPDATE Trips
+	SET RoomId = @TargetRoomId
+	WHERE Id = @TripId
+
+GO
+----------------------------- For testing ------------------------------
+EXEC usp_SwitchRoom 10, 11
+EXEC usp_SwitchRoom 10, 7
+EXEC usp_SwitchRoom 10, 8
+------------------------------------------------------------------------
