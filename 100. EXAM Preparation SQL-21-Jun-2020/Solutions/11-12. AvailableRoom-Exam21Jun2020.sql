15f02a38ca9ca41d338bd301e9d34aaf56fd3bd9
Mironov <mironov5712s@gmail.com>
2021-02-16 15:26:34 +0200

    Update 11-12. AvailableRoom-Exam21Jun2020.sql

--- 100. EXAM Preparation SQL-21-Jun-2020/Solutions/11-12. AvailableRoom-Exam21Jun2020.sql
+++ 100. EXAM Preparation SQL-21-Jun-2020/Solutions/11-12. AvailableRoom-Exam21Jun2020.sql
@@ -24,3 +24,31 @@ GO
 SELECT dbo.udf_GetAvailableRoom(112, '2011-12-17', 2)
 SELECT dbo.udf_GetAvailableRoom(94, '2015-07-26', 3)
 --------------------------------------------------------------------
+GO
+CREATE OR ALTER FUNCTION udf_GetAvailableRoom(@HotelId INT, @Date DATE, @People INT)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+DECLARE @roomID INT;
+SET @roomID = (SELECT TOP(1) r.Id 
+				FROM Rooms AS r
+				JOIN Trips AS tr ON tr.RoomId = r.Id
+				WHERE (@Date NOT BETWEEN tr.ArrivalDate AND tr.ReturnDate)
+						AND (YEAR(@Date) = YEAR(tr.ArrivalDate))
+						AND (tr.CancelDate IS NULL)
+						AND (r.Beds >= @People)
+						AND (r.HotelId = @HotelId)
+				ORDER BY r.Price DESC
+				)
+IF (@roomID IS NULL)
+	RETURN 'No rooms available'
+
+DECLARE @roomType NVARCHAR(20) = (SELECT [Type] FROM Rooms WHERE Id = @roomID);
+DECLARE @roomBeds INT = (SELECT Beds FROM Rooms WHERE Id = @roomID);
+DECLARE @hotelBaseRate DECIMAL(17, 2) = (SELECT BaseRate FROM Hotels WHERE Id = @HotelId);
+DECLARE @roomPrice DECIMAL(17, 2) = (SELECT Price FROM Rooms WHERE Id = @roomID);
+
+DECLARE @totalPrice DECIMAL(17, 2) = (@hotelBaseRate + @roomPrice) * @People;
+
+RETURN CONCAT('Room ', @roomID, ': ', @roomType, ' (', @roomBeds, ' beds) - $', @totalPrice);
+END
