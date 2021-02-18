eae3dd1958b8d62781b76d3bdf6cdb6b4b21106e
Mironov <mironov5712s@gmail.com>
2021-02-18 15:33:50 +0200

    Update 05. Mechanic-Exam27Jun2020.sql

--- 101. EXAM Preparation SQL-27-Jun-2020/Solutions/05. Mechanic-Exam27Jun2020.sql
+++ 101. EXAM Preparation SQL-27-Jun-2020/Solutions/05. Mechanic-Exam27Jun2020.sql
@@ -5,5 +5,15 @@ SELECT (m.FirstName + ' ' + m.LastName) AS Mechanic, [Status], IssueDate
 	JOIN Jobs AS j ON j.MechanicId = m.MechanicId
 	ORDER BY m.MechanicId, IssueDate, j.JobId
 
+-- 06. Current Clients
+
+SELECT (c.FirstName + ' ' + c.LastName) AS [Client], 
+		(DATEDIFF(DAY, IssueDate, '2017-04-24')) AS [Days going], [Status]
+	FROM Clients AS c
+	JOIN Jobs AS j ON j.ClientId = c.ClientId
+	WHERE j.Status != 'Finished'
+	ORDER BY [Days going] DESC, c.ClientId
+
+
 
 
