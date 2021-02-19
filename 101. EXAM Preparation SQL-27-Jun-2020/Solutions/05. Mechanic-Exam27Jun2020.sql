12ebe57540b3722f6cf58e8b402fcd76b6937698
Mironov <mironov5712s@gmail.com>
2021-02-19 11:36:42 +0200

    Update 05. Mechanic-Exam27Jun2020.sql

--- 101. EXAM Preparation SQL-27-Jun-2020/Solutions/05. Mechanic-Exam27Jun2020.sql
+++ 101. EXAM Preparation SQL-27-Jun-2020/Solutions/05. Mechanic-Exam27Jun2020.sql
@@ -47,6 +47,16 @@ SELECT (m.FirstName + ' ' + m.LastName) AS Available
 										GROUP BY ms.MechanicId)
 	GROUP BY m.MechanicId, m.FirstName, m.LastName
 
+-- 09. Past Expenses
+
+SELECT j.JobId, ISNULL(SUM(op.Quantity * p.Price), 0) AS Total
+	FROM Jobs AS j
+	LEFT JOIN Orders AS o ON o.JobId = j.JobId
+	LEFT JOIN OrderParts AS op ON op.OrderId = o.OrderId
+	LEFT JOIN Parts AS p ON p.PartId = op.PartId
+	WHERE j.Status = 'Finished'
+	GROUP BY j.JobId
+	ORDER BY Total DESC, JobId
 
 
 
