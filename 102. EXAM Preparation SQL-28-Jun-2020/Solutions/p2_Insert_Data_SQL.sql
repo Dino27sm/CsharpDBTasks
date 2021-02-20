4076809e0388a4ea6da072b856d775f639c17c3c
Mironov <mironov5712s@gmail.com>
2021-02-20 12:04:35 +0200

    Update p2_Insert_Data_SQL.sql

--- 102. EXAM Preparation SQL-28-Jun-2020/Solutions/p2_Insert_Data_SQL.sql
+++ 102. EXAM Preparation SQL-28-Jun-2020/Solutions/p2_Insert_Data_SQL.sql
@@ -27,3 +27,18 @@ UPDATE [Spaceships]
 DELETE FROM [TravelCards] WHERE JourneyId BETWEEN 1 AND 3
 DELETE FROM Journeys WHERE Id BETWEEN 1 AND 3
 
+-- 05. Select All Military Journeys
+
+-----  FIRST CLOSE ALL BATCH FILES THAT USE DATABASE ColonialJourney
+--GO
+--USE master
+--DROP DATABASE ColonialJourney
+--RESTORE DATABASE ColonialJourney FROM DISK = 'E:\Soft-Uni\BackUpDB\ColonialJourney_backup.bak'
+
+
+SELECT Id, FORMAT(JourneyStart, 'dd/MM/yyyy') AS JourneyStart
+			,FORMAT(JourneyEnd, 'dd/MM/yyyy') AS JourneyEnd
+	FROM Journeys
+	WHERE Purpose = 'Military'
+	ORDER BY JourneyStart
+
