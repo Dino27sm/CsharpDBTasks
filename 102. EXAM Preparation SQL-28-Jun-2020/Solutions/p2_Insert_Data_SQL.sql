905ed31537cdfa4a423ee616ddb789913f7c576b
Mironov <mironov5712s@gmail.com>
2021-02-20 12:03:44 +0200

    Update p2_Insert_Data_SQL.sql

--- 102. EXAM Preparation SQL-28-Jun-2020/Solutions/p2_Insert_Data_SQL.sql
+++ 102. EXAM Preparation SQL-28-Jun-2020/Solutions/p2_Insert_Data_SQL.sql
@@ -22,3 +22,8 @@ UPDATE [Spaceships]
 	SET [LightSpeedRate] += 1
 	WHERE Id BETWEEN 8 AND 12
 
+-- 04. Delete
+
+DELETE FROM [TravelCards] WHERE JourneyId BETWEEN 1 AND 3
+DELETE FROM Journeys WHERE Id BETWEEN 1 AND 3
+
