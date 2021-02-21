dd331aa28987954ecf5e93268f717e8c17379157
Mironov <mironov5712s@gmail.com>
2021-02-21 05:25:01 +0200

    Update p2_Insert_Data_SQL.sql

--- 102. EXAM Preparation SQL-28-Jun-2020/Solutions/p2_Insert_Data_SQL.sql
+++ 102. EXAM Preparation SQL-28-Jun-2020/Solutions/p2_Insert_Data_SQL.sql
@@ -65,3 +65,14 @@ SELECT COUNT(*) AS [Count]
 	JOIN Journeys AS j ON j.Id = tc.JourneyId
 	WHERE j.Purpose = 'Technical'
 
+-- 08. Select Spaceships With Pilots
+
+SELECT s.[Name], s.Manufacturer
+	FROM Spaceships AS s
+	JOIN Journeys AS j ON j.SpaceshipId = s.Id
+	JOIN TravelCards AS tc ON tc.JourneyId = j.Id
+	JOIN Colonists AS c ON c.Id = tc.ColonistId
+	WHERE tc.JobDuringJourney = 'Pilot'
+			AND (DATEDIFF(YEAR, c.BirthDate, '01/01/2019') <= 30)
+	ORDER BY s.[Name]
+
