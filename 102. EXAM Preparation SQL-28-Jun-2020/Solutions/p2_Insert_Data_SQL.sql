342ea321c9d70fe2be3de68db24583baf4f73201
Mironov <mironov5712s@gmail.com>
2021-02-23 18:58:23 +0200

    Update p2_Insert_Data_SQL.sql

--- 102. EXAM Preparation SQL-28-Jun-2020/Solutions/p2_Insert_Data_SQL.sql
+++ 102. EXAM Preparation SQL-28-Jun-2020/Solutions/p2_Insert_Data_SQL.sql
@@ -76,3 +76,11 @@ SELECT s.[Name], s.Manufacturer
 			AND (DATEDIFF(YEAR, c.BirthDate, '01/01/2019') <= 30)
 	ORDER BY s.[Name]
 
+-- 09. Planets And Journeys
+
+SELECT p.[Name] AS PlanetName, COUNT(*) AS JourneysCount
+	FROM Planets AS p
+	JOIN Spaceports AS sp ON sp.PlanetId = p.Id
+	JOIN Journeys AS j ON j.DestinationSpaceportId = sp.Id
+	GROUP BY p.[Name]
+	ORDER BY COUNT(*) DESC, p.[Name]
