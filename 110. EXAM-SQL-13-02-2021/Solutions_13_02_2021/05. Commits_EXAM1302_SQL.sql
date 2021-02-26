ea1413d1f10571c5d3990b059a78a5875efd8fc4
Mironov <mironov5712s@gmail.com>
2021-02-26 11:56:25 +0200

    Update 05. Commits_EXAM1302_SQL.sql

--- 110. EXAM-SQL-13-02-2021/Solutions_13_02_2021/05. Commits_EXAM1302_SQL.sql
+++ 110. EXAM-SQL-13-02-2021/Solutions_13_02_2021/05. Commits_EXAM1302_SQL.sql
@@ -4,3 +4,10 @@ SELECT Id, [Message], RepositoryId, ContributorId
 	FROM Commits
 	ORDER BY Id, [Message], RepositoryId, ContributorId
 
+-- 06. Front-end
+
+SELECT Id, [Name], [Size]
+	FROM Files
+	WHERE Size > 1000 AND [Name] LIKE '%html%'
+	ORDER BY [Size] DESC, Id, [Name]
+
