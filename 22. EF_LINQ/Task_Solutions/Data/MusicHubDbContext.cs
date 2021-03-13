5507b1271759da124fe530dabfd41188e215563c
Mironov <mironov5712s@gmail.com>
2021-03-13 14:31:45 +0200

    Part of Models

---
+++ 22. EF_LINQ/Task_Solutions/Data/MusicHubDbContext.cs
@@ -0,0 +1,76 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+
+        }
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongsPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer("Server=.; Database=MusicHub; Integrated Security=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>(entity =>
+            {
+                entity
+                .HasOne(a => a.Album)
+                .WithMany(s => s.Songs)
+                .HasForeignKey(a => a.AlbumId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+                entity
+                .HasOne(w => w.Writer)
+                .WithMany(s => s.Songs)
+                .HasForeignKey(w => w.WriterId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Album>(entity =>
+            {
+                entity
+                .HasOne(p => p.Producer)
+                .WithMany(a => a.Albums)
+                .HasForeignKey(p => p.ProducerId);
+            });
+
+            modelBuilder.Entity<SongPerformer>(entity =>
+            {
+                entity
+                .HasOne(s => s.Song)
+                .WithMany(sp => sp.SongPerformers)
+                .HasForeignKey(s => s.SongId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+                entity
+                .HasOne(p => p.Performer)
+                .WithMany(ps => ps.PerformerSongs)
+                .HasForeignKey(p => p.PerformerId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+                entity
+                .HasKey(pk => new { pk.SongId, pk.PerformerId});
+            });
+        }
+    }
+}
