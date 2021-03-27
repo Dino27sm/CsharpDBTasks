dc5cb1b5bd9330963313fb097335d0444474f94b
Mironov <mironov5712s@gmail.com>
2021-03-27 19:37:09 +0200

    Models

---
+++ 25. EF_JSON Processing/01. Products_Shop_Database/ProductShop/DataTransferObjects/ProductsOutputInRange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.DataTransferObjects
+{
+    public class ProductsOutputInRange
+    {
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public string seller { get; set; }
+    }
+}
