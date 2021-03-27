483770d027161bf3a85e5ccce7a4c819ef120709
Mironov <mironov5712s@gmail.com>
2021-03-27 19:42:56 +0200

    Create ProductShopProfile.cs

---
+++ 25. EF_JSON Processing/01. Products_Shop_Database/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+    using System.Linq;
+    using ProductShop.DataTransferObjects;
+    using ProductShop.Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<UserInputModel, User>();
+            CreateMap<ProductInputModel, Product>();
+            CreateMap<CategoryInputModel, Category>();
+            CreateMap<CategoryProductInputModel, CategoryProduct>();
+
+            CreateMap<Product, ProductsOutputInRange>()
+                .ForMember(x => x.seller, y =>
+                    y.MapFrom(z => string.Join(" ", z.Seller.FirstName, z.Seller.LastName)));
+        }
+    }
+}
