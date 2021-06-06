a8c5232b894cdaef6ee1a98934e7c1865bd104ce
Mironov <mironov5712s@gmail.com>
2021-06-06 11:48:37 +0300

    Create ImportUserCardDtoJSON.cs

---
+++ 105. EF_Exam Preparation - 08 Aug 2020/Solutions/DataProcessor/Dto/Import/ImportUserCardDtoJSON.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using VaporStore.Data.Models;
+
+namespace VaporStore.DataProcessor.Dto.Import
+{
+    public class ImportUserCardDtoJSON
+    {
+        [Required]
+        [RegularExpression(@"[A-Z][a-z]+\s[A-Z][a-z]+")]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Range(3, 103)]
+        public int Age { get; set; }
+
+        public List<ImportCardDtoJSON> Cards { get; set; }
+    }
+
+    public class ImportCardDtoJSON
+    {
+        [Required]
+        [RegularExpression(@"(\d{4}\s){3}\d{4}")]
+        public string Number { get; set; }
+
+        [Required]
+        [RegularExpression(@"\b\d{3}\b")]
+        public string Cvc { get; set; }
+
+        [EnumDataType(typeof(CardType))]
+        public string Type { get; set; }
+    }
+}
