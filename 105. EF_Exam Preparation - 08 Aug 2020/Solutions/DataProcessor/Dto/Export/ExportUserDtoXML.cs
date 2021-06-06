778d0d4ed3f3796d77de2338503993bac8a23c06
Mironov <mironov5712s@gmail.com>
2021-06-06 11:49:32 +0300

    Create ExportUserDtoXML.cs

---
+++ 105. EF_Exam Preparation - 08 Aug 2020/Solutions/DataProcessor/Dto/Export/ExportUserDtoXML.cs
@@ -0,0 +1,48 @@
+﻿namespace VaporStore.DataProcessor.Dto.Export
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    [XmlType("User")]
+    public class ExportUserDtoXML
+    {
+        [XmlAttribute("username")]
+        public string Username { get; set; }
+
+        [XmlArray("Purchases")]
+        public List<CardDto> Purchases { get; set; }
+
+        [XmlElement("TotalSpent")]
+        public decimal TotalSpent { get; set; }
+    }
+
+    [XmlType("Purchase")]
+    public class CardDto
+    {
+        [XmlElement("Card")]
+        public string CardNumber { get; set; }
+
+        [XmlElement("Cvc")]
+        public string CvcNumber { get; set; }
+
+        [XmlElement("Date")]
+        public string Date { get; set; }
+
+        [XmlElement("Game")]
+        public GameDto Game { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Game")]
+    public class GameDto
+    {
+        [XmlAttribute(AttributeName = "title")]
+        public string Title { get; set; }
+
+        [XmlElement(ElementName = "Genre")]
+        public string GenreName { get; set; }
+
+        [XmlElement(ElementName = "Price")]
+        public decimal Price { get; set; }
+    }
+}
