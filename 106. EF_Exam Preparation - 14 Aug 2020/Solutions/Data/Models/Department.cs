48a6d4e89fde653b7632e639fca9c059db139955
Mironov <mironov5712s@gmail.com>
2021-06-13 07:26:36 +0300

    Create Department.cs

---
+++ 106. EF_Exam Preparation - 14 Aug 2020/Solutions/Data/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿namespace SoftJail.Data.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
+
+    public class Department
+    {
+        [Range(1, 1000)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Cell> Cells { get; set; } = new HashSet<Cell>();
+        public ICollection<Officer> Officers { get; set; } = new HashSet<Officer>();
+    }
+}
+//•	Id – integer, Primary Key
+//•	Name – text with min length 3 and max length 25 (required)
+//•	Cells - collection of type Cell
