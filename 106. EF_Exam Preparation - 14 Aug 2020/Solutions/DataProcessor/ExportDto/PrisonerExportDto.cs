f793321e88ddc6cc47951d201d751b62fe8c1912
Mironov <mironov5712s@gmail.com>
2021-06-14 12:21:37 +0300

    Create PrisonerExportDto.cs

---
+++ 106. EF_Exam Preparation - 14 Aug 2020/Solutions/DataProcessor/ExportDto/PrisonerExportDto.cs
@@ -0,0 +1,22 @@
+﻿namespace SoftJail.DataProcessor.ExportDto
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class PrisonerExportDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? CellNumber { get; set; }
+        public ICollection<OfficerDto> Officers { get; set; }
+        public decimal TotalOfficerSalary { get; set; }
+    }
+
+    public class OfficerDto
+    {
+        [Required]
+        public string OfficerName { get; set; }
+        public string Department { get; set; }
+    }
+}
