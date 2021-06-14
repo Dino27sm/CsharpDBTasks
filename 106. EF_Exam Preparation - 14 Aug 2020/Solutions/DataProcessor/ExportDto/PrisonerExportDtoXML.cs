ec97b44498200cd22670d313f208fb6694fb28ac
Mironov <mironov5712s@gmail.com>
2021-06-14 12:22:05 +0300

    Create PrisonerExportDtoXML.cs

---
+++ 106. EF_Exam Preparation - 14 Aug 2020/Solutions/DataProcessor/ExportDto/PrisonerExportDtoXML.cs
@@ -0,0 +1,29 @@
+﻿namespace SoftJail.DataProcessor.ExportDto
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Prisoner")]
+    public class PrisonerExportDtoXML
+    {
+        [XmlElement("Id")]
+        public int Id { get; set; }
+
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("IncarcerationDate")]
+        public string IncarcerationDate { get; set; }
+
+        [XmlArray("EncryptedMessages")]
+        public List<MailDtoXML> MailMessages { get; set; }
+    }
+
+    [XmlType("Message")]
+    public class MailDtoXML
+    {
+        [XmlElement("Description")]
+        public string Description { get; set; }
+    }
+}
