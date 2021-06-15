a547675398d436f55202f5ad8c25c37c23bf0f24
Mironov <mironov5712s@gmail.com>
2021-06-15 11:23:34 +0300

    Create OfficerPrisonerImportDtoXML.cs

---
+++ 106. EF_Exam Preparation - 14 Aug 2020/Solutions/ImportDto/OfficerPrisonerImportDtoXML.cs
@@ -0,0 +1,41 @@
+﻿namespace SoftJail.DataProcessor.ImportDto
+{
+    using SoftJail.Data.Models.Enums;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Officer")]
+    public class OfficerPrisonerImportDtoXML
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        [XmlElement("Name")]
+        public string FullName { get; set; }
+
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        [XmlElement("Money")]
+        public decimal Money { get; set; }
+
+        [EnumDataType(typeof(Position))]
+        [XmlElement("Position")]
+        public string Position { get; set; }
+
+        [EnumDataType(typeof(Weapon))]
+        [XmlElement("Weapon")]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public List<OfficerPrisonerDto> Prisoners { get; set; }
+    }
+
+    [XmlType("Prisoner")]
+    public class OfficerPrisonerDto
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+    }
+}
