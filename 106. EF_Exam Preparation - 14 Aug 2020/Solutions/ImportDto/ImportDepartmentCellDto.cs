32add570c69a8689c6f85ad35e2fe442fc9e9ff3
Mironov <mironov5712s@gmail.com>
2021-06-15 11:24:06 +0300

    Create ImportDepartmentCellDto.cs

---
+++ 106. EF_Exam Preparation - 14 Aug 2020/Solutions/ImportDto/ImportDepartmentCellDto.cs
@@ -0,0 +1,23 @@
+﻿namespace SoftJail.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
+
+    public class ImportDepartmentCellDto
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public List<ImportCellDto> Cells { get; set; }
+    }
+
+    public class ImportCellDto
+    {
+        [Required]
+        [Range(1, 1000)]
+        public int CellNumber { get; set; }
+
+        public bool HasWindow { get; set; }
+    }
+}
