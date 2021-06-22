5c70e384e0493292b4dc38ab67d225caa82c7905
Mironov <mironov5712s@gmail.com>
2021-06-22 09:44:32 +0300

    Create ExportOldestBookDtoXML.cs

---
+++ 107. EF_Exam Preparation - 13 Dec 2019/Solutions/DataProcessor/ExportDto/ExportOldestBookDtoXML.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BookShop.DataProcessor.ExportDto
+{
+    [XmlType("Book")]
+    public class ExportOldestBookDtoXML
+    {
+        [XmlAttribute("Pages")]
+        public string BookPages { get; set; }
+
+        [XmlElement("Name")]
+        public string BookName { get; set; }
+
+        [XmlElement("Date")]
+        public string BookPublished { get; set; }
+    }
+}
