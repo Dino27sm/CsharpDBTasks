e9faa5f07e9953dee590d79ef3d0056b76df32a7
Mironov <mironov5712s@gmail.com>
2021-06-22 09:45:23 +0300

    Create ImportAuthorDtoJSON.cs

---
+++ 107. EF_Exam Preparation - 13 Dec 2019/Solutions/DataProcessor/ImportDto/ImportAuthorDtoJSON.cs
@@ -0,0 +1,42 @@
+﻿using BookShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    public class ImportAuthorDtoJSON
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"\b\d{3}-\d{3}-\d{4}\b")]
+        public string Phone { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public List<BookAuthorDto> Books { get; set; }
+    }
+
+    public class BookAuthorDto 
+    { 
+        public int? Id { get; set; } 
+    }
+}
+//•	Id - integer, Primary Key
+//•	FirstName - text with length [3, 30]. (required)
+//•	LastName - text with length[3, 30]. (required)
+//•	Email - text(required).Validate it! There is attribute for this job.
+//•	Phone - text.Consists only of three groups(separated by '-'),
+//          the first two consist of three digits and the last one - of 4 digits. (required)
+//•	AuthorsBooks - collection of type AuthorBook
